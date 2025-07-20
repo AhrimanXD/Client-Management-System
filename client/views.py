12aeb713b5572269845cc0760af4fa7edfcdd802
AhrimanXD <neoxd405@gmail.com>
2025-07-20 18:28:05 +0100

    Created The Client List View

--- client/views.py
+++ client/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+#from django.shortcuts import render
+from .models import Client
+#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+
+class ClientListView(LoginRequiredMixin,ListView):
+  template_name = 'client/client_list.html'
+  model = Client
+  context_object_name = 'clients'
+  
+
+  def get_queryset(self) -> QuerySet[Any]:
+    return Client.objects.filter(user=self.request.user)
+
+
+# Function based View for client list
+"""@login_required
+def client_list_view(request):
+  clients = Client.objects.filter(user = request.user)
+  template_name = 'client/client_list.html'
+  context = {
+    "clients":clients
+  }
+  return render(request,template_name,context)"""
