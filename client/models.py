5f14c2b904cff234810189f79b0f37d330336df4
AhrimanXD <neoxd405@gmail.com>
2025-07-20 17:23:47 +0100

    Updated Client Model to allow to separate to have clients with the same email and phone_number

--- client/models.py
+++ client/models.py
@@ -9,10 +9,21 @@ class Client(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='clients')
 
 class ClientEmail(models.Model):
-  email = models.EmailField(unique=True)
-  client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='emails')
+    email = models.EmailField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='emails')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['client', 'email'], name='unique_email_per_client')
+        ]
+
 
 class ClientPhoneNumber(models.Model):
   phone_number = models.CharField(max_length=50)
   client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='phonenumbers')
+
+  class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['client', 'phone_number'], name='unique_phone_number_per_client')
+        ]
 
