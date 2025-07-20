a8bc9e97440841ed6b984b0c41037da71d910baf
AhrimanXD <neoxd405@gmail.com>
2025-07-20 20:28:54 +0100

    Added Client Detail Page

--- client/views.py
+++ client/views.py
@@ -4,7 +4,7 @@ from django.db.models.query import QuerySet
 from .models import Client
 #from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class ClientListView(LoginRequiredMixin,ListView):
@@ -16,6 +16,17 @@ class ClientListView(LoginRequiredMixin,ListView):
   def get_queryset(self) -> QuerySet[Any]:
     return Client.objects.filter(user=self.request.user)
 
+class ClientDetailView(LoginRequiredMixin, DetailView):
+  template_name = "client/client_detail.html"
+  model = Client
+  context_object_name = 'client'
+
+  def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    context = super().get_context_data(**kwargs)
+    client = self.get_object()
+    context['emails'] = client.emails.all()
+    context['phone_numbers'] = client.phonenumbers.all()
+    return context
 
 # Function based View for client list
 """@login_required
